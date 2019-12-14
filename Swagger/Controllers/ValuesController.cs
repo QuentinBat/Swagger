65c561719686d22eb3abc90bf61a7bae20dbef92
BATON Quentin <213558@supinfo.com>
2019-12-14 21:53:14 +0100

    Ajoutez des fichiers projet.

---
+++ Swagger/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Swagger.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class ValuesController : Controller
+    {
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Gets two values", Description = "Gets two hardcoded values")]
+        [SwaggerResponse(200, "I guess everything worked")]
+        [SwaggerResponse(400, "BAD REQUEST")]
+        public IEnumerable<string> Values()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Gets a value.
+        /// </summary>
+        /// <param name="id">The id !!</param>x
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Posts a value.
+        /// </summary>
+        /// <param name="value">The value !!</param>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Puts a value.
+        /// </summary>
+        /// <param name="id">The id !!</param>
+        /// <param name="value">The value !!</param>
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Deletes an entry.
+        /// </summary>
+        /// <param name="id">The id !!</param>
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ Swagger/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.IO;
+using Swashbuckle.AspNetCore.Swagger;
+using Newtonsoft.Json;
+
+namespace Swagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 .AddJsonOptions(options =>
+                 {
+                     options.SerializerSettings.Formatting = Formatting.Indented;
+                 });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "PrintFramer API",
+                    Description = "Calculates the cost of a picture frame based on its dimensions.",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Your name",
+                        Email = string.Empty,
+                        Url = "https://www.microsoft.com/learn"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                c.EnableAnnotations();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
